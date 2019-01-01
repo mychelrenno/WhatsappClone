4f3c3c0f9a4d09d09fa418de32ce844c789c309d
mychelrenno <mychelrenno@gmail.com>
2019-01-01 20:46:49 -0200

    aula 167 - utilizando máscaras para telefones

--- app/src/main/java/br/com/whatsappandroid/crusoandroid/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/br/com/whatsappandroid/crusoandroid/whatsapp/activity/LoginActivity.java
@@ -2,13 +2,40 @@ package br.com.whatsappandroid.crusoandroid.whatsapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
+
 import br.com.whatsappandroid.crusoandroid.whatsapp.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText nome;
+    private EditText codPais;
+    private EditText codArea;
+    private EditText telefone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        nome = findViewById(R.id.edit_nome);
+        codPais = findViewById(R.id.edit_cod_pais);
+        codArea = findViewById(R.id.edit_cod_area);
+        telefone = findViewById(R.id.edit_telefone);
+
+        SimpleMaskFormatter smfCodPais = new SimpleMaskFormatter("+NN");
+        MaskTextWatcher mtwCodPais = new MaskTextWatcher(codPais, smfCodPais);
+        codPais.addTextChangedListener(mtwCodPais);
+
+        SimpleMaskFormatter smfCodArea = new SimpleMaskFormatter("(NN)");
+        MaskTextWatcher mtwCodArea = new MaskTextWatcher(codArea, smfCodArea);
+        codArea.addTextChangedListener(mtwCodArea);
+
+        SimpleMaskFormatter smfTelefone = new SimpleMaskFormatter("NNNNN-NNNN");
+        MaskTextWatcher mtwTelefone = new MaskTextWatcher(telefone, smfTelefone);
+        telefone.addTextChangedListener(mtwTelefone);
     }
 }
